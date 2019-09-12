4f41dc77e0f63f8a3cb706c2d63c0d4fc9b89d4f
Rahul Shirphule <Rahul.Shirphule@fnb.co.za>
2019-09-12 18:12:05 +0200

    initial commit

---
+++ Entersekt-asses/Entersekt-asses/Entersekt_asses.h
@@ -0,0 +1,19 @@
+//
+//  Entersekt_asses.h
+//  Entersekt-asses
+//
+//  Created by Rahul Shirphule on 2019/09/12.
+//  Copyright © 2019 Entersekt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Entersekt_asses.
+FOUNDATION_EXPORT double Entersekt_assesVersionNumber;
+
+//! Project version string for Entersekt_asses.
+FOUNDATION_EXPORT const unsigned char Entersekt_assesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Entersekt_asses/PublicHeader.h>
+
+
